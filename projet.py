9b08d7f35f5e53cf9a3da77c43578819ff10c1b3
Vivien GAGLIANO <gagliano.vivien@gmail.com>
2021-04-27 14:50:15 +0200

    Completed silhouette computing algorithm

--- projet.py
+++ projet.py
@@ -7,6 +7,7 @@
 
 # %% Importing libraries
 import io, re
+import math
 from contextlib import redirect_stdout
 import random
 import numpy
@@ -86,46 +87,35 @@ for cluster_num in numpy.unique(kmeans.labels_):
 # %% Silhouette index : Mesurer la qualité du Clustering avec l’indice de la Silhouette
 
 # Compute matrix of distances between points
-def compute_point_distances():
-    distances = numpy.zeros(train_data_x.shape)
-    for i in range(train_data_x.shape[0]):
-        for j in range(i):
-            dist = 0
-            for l in range(64):
-                dist += (train_data_x[i, l] - train_data_x[j, l])**2
-            distances[i, j] = distances[j, i] = numpy.sqrt(dist)
-    return distances
+point_distances = numpy.zeros(train_data_x.shape)
+for i in range(train_data_x.shape[0]):
+    for j in range(i):
+        dist = 0
+        for l in range(64):
+            dist += (train_data_x[i, l] - train_data_x[j, l])**2
+        point_distances[i, j] = point_distances[j, i] = numpy.sqrt(dist)
 
 # Compute array of distances between point at given index and points of other clusters
 def compute_cluster_distances(index):
-    point_distances = compute_point_distances(train_data_x)
     distances = []
-    for i in range(K):
-
-# J'EN ETAIS LA
-
-
-
-
-def silhouette():
-    distances = compute_point_distances(train_data_x)
-    sil = []
     for i in range(kmeans.labels.len):
-        same_cluster = []
-        diff_cluster = []
-        for j in range(kmeans.labels.len):
-            if i == j:
-                break
-            if kmeans.labels[i] == kmeans.labels[j]:
-                same_cluster.append(distances[j])
-            else:
-                diff_cluster.append(distances[j])
-        # distance moyenne entre i et les points de son cluster
-        a_i = sum(same_cluster) / same_cluster.len
-        b_i = # minimum des distances moyennes entre i et les points de chaque autre cluster
-        sil[i] = (b_i - a_i) / max(a_i, b_i)
+        distances[kmeans.labels[i]].append(point_distances[index, i])
+    mean_distances = []
+    for arr in distances:
+        mean_distances.append(sum(arr) / arr.len)
+    return mean_distances
 
-    return
+# Compute silhouette for point at given index
+def point_silhouette(index):
+    cluster_distances = compute_cluster_distances(index)
+    a_i = cluster_distances(kmeans.labels[index]) # mean distance between index point and other points of its cluster
+    b_i = math.inf # minimum of mean distances between index point and other clusters' points
+    for dist in [cluster_distances[j] for j in range(cluster_distances.len) if j != kmeans.labels[index]]: # we read through the cluster mean distance array, and skip the point's own cluster
+        b_i = math.min(b_i, dist)
+    return (b_i - a_i) / math.max(a_i, b_i)
 
+# Compute the clustering's overall silhouette
+silhouette = sum([point_silhouette(i) for i in range(kmeans.labels.len)]) / kmeans.labels.len
 
+print(silhouette)
 # %%
